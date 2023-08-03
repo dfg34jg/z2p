aeb1a1cb2c79c8f8862ea9da0f6ee9b2eb8031e0
dfg34jg <141349603+dfg34jg@users.noreply.github.com>
2023-08-03 13:51:41 -0600

    refactor main.rs to handle lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind");
+
+    z2p::run(listener)?.await
 }
